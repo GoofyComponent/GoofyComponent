632378cebfc2ba4c80573405adb5859cec692160
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2022-09-05 19:51:01 +0200

    Add Collapse component

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,7 @@
 <script>
-  
+  import Collapse from './UI/Collapse/Collapse.svelte';
 </script>
+
+<Collapse sectionName={"Section 1"} sectionContent={"Section 1 content"} />
+<Collapse sectionName={"Section 2"} sectionContent={"Section 2 content"} />
+<p>test</p>
---
+++ src/UI/Collapse/Collapse.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "./Collapse.css";
+    import FaAngleDown from 'svelte-icons/fa/FaAngleDown.svelte';
+    export let sectionName;
+    export let sectionContent;
+    let isOpen = false;
+</script>
+
+<section id="collapseContainer">
+    <div class="collapseButton noSelect" on:click={() => {isOpen = !isOpen}} class:open="{isOpen === true}">
+        <h2>{sectionName}</h2> 
+        <div class="icon">
+            <FaAngleDown />
+        </div>
+    </div>
+    <div class="collapseContent" class:open="{isOpen === true}">
+        <p>{sectionContent}</p>
+    </div>   
+</section>
