7cb10aabdf948ca9737db446a43c6f2f4114d765
Hiteazel <khacminhbrandon.vo@gmail.com>
2022-09-05 16:36:05 +0200

    Tabs

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,23 @@
 <script>
-  import Review from "./ui/Review/Review.svelte";
+  import Review from './UI/review/review.svelte';
+	import { Tabs, TabList, TabPanel, Tab } from './UI/Tab/tabs.js';
 </script>
 
 <div>
-  <Review rating="45" />
+  <Tabs>
+    <TabList>
+      <Tab>
+        Review
+      </Tab>
+      <Tab>
+        Collapse
+      </Tab>
+    </TabList>
+    <TabPanel>
+      <Review rating="45" />
+    </TabPanel>
+    <TabPanel>
+      ANTOINE
+    </TabPanel>
+  </Tabs>
 </div>
