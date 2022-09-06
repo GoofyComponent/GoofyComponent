4d87088d7075544eda5b4206902b8afde7c8844e
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2022-09-06 19:10:37 +0200

    1ere Integration  des components

--- src/Placeholder.svelte
+++ src/Placeholder.svelte
@@ -1,7 +1,17 @@
 <script>
+  import DiGithubBadge from "svelte-icons/di/DiGithubBadge.svelte";
+
   import AlertModal from "./UI/AlertModal/AlertModal.svelte";
   import Collapse from "./UI/Collapse/Collapse.svelte";
-  import DiGithubBadge from "svelte-icons/di/DiGithubBadge.svelte";
+  import InputText from "./UI/inputText/InputText.svelte";
+  import Review from "./UI/Review/Review.svelte";
+  import Check from "./UI/checkBox/Check.svelte";
+  import Color from "./UI/Color/Color.svelte";
+  import Calc from "./UI/Calc/Calc.svelte";
+  import TabList from "./UI/Tab/TabList.svelte";
+  import Tab from "./UI/Tab/Tab.svelte";
+  import TabPanel from "./UI/Tab/TabPanel.svelte";
+  import Tabs from "./UI/Tab/Tabs.svelte";
   let btnTrigger = false;
 </script>
 
@@ -18,11 +28,9 @@
 <header>
   <div class="text">
     <h1>GoofyComponents</h1>
-    <h2>The Svelte Component Library</h2>
-    <h3>"AUUUGGHHHH"</h3>
+    <h2>The "AUUUGGHHHH" Svelte Component Library</h2>
   </div>
   <div class="icon">
-    <div class="circle" />
     <img src="/goofyicon.png" alt="Goofy" />
   </div>
 </header>
@@ -75,7 +83,23 @@
         document.getElementById("color").scrollIntoView();
       }}
     >
-      <p>Color</p>
+      <p>ColorPicker</p>
+    </div>
+    <div
+      on:click={(e) => {
+        e.preventDefault();
+        document.getElementById("textinput").scrollIntoView();
+      }}
+    >
+      <p>Text Input</p>
+    </div>
+    <div
+      on:click={(e) => {
+        e.preventDefault();
+        document.getElementById("checkbox").scrollIntoView();
+      }}
+    >
+      <p>Checkbox</p>
     </div>
   </nav>
 
@@ -89,6 +113,7 @@
         exercitationem sint asperiores, blanditiis accusantium iusto saepe
         repudiandae eaque officia dicta itaque.
       </p>
+      <p>Antoine</p>
     </div>
 
     <div class="compoZone">
@@ -96,7 +121,7 @@
         on:click={() => {
           btnTrigger = !btnTrigger;
         }}
-        class="alertModalBtn">Cliquez-moi</button
+        class="alertModalBtn">Cliquez-moi !</button
       >
       {#if btnTrigger}
         <AlertModal
@@ -146,7 +171,14 @@
       </p>
     </div>
 
-    <div class="compoZone" />
+    <div class="compoZone">
+      <Review
+        name={"Jean-Louis"}
+        rating={4}
+        src="/Louis.jpeg"
+        comments={"Le Restaurant sert un très bon Ricard. Le service est très bon. Le cadre est très bon. Le prix est très bon. Et la serveuse..."}
+      />
+    </div>
   </div>
   <div class="displayCompoContain" id="tabs">
     <div class="text">
@@ -160,7 +192,33 @@
       </p>
     </div>
 
-    <div class="compoZone" />
+    <div class="compoZone">
+      <Tabs>
+        <TabList>
+          <Tab>Nouveau vlog au japon</Tab>
+          <Tab>Le dernier VLOG + Video</Tab>
+        </TabList>
+        <TabPanel>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptates, quae, quod, voluptas quibusdam voluptatibus quidem
+            voluptatum quos quia quas quod. Quisquam, quae. Quisquam, quae.
+            Quisquam, quae. Quisquam, quae.
+          </p>
+        </TabPanel>
+        <TabPanel>
+          <p>C'est mon dernier vlog</p>
+          <iframe
+            width="300"
+            height="200"
+            src="https://www.youtube.com/embed/hseE17LTYfc?start=60"
+            title="JAPON"
+            frameborder="0"
+            allowfullscreen
+          />
+        </TabPanel>
+      </Tabs>
+    </div>
   </div>
   <div class="displayCompoContain" id="calc">
     <div class="text">
@@ -174,11 +232,13 @@
       </p>
     </div>
 
-    <div class="compoZone" />
+    <div class="compoZone">
+      <Calc />
+    </div>
   </div>
   <div class="displayCompoContain" id="color">
     <div class="text">
-      <h4>Color</h4>
+      <h4>ColorPicker</h4>
       <p>Utilisation</p>
       <p>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
@@ -188,6 +248,40 @@
       </p>
     </div>
 
-    <div class="compoZone" />
+    <div class="compoZone">
+      <Color />
+    </div>
+  </div>
+  <div class="displayCompoContain" id="textinput">
+    <div class="text">
+      <h4>Text Input</h4>
+      <p>Utilisation</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+        atque error, quos corrupti enim qui deserunt veniam perferendis
+        exercitationem sint asperiores, blanditiis accusantium iusto saepe
+        repudiandae eaque officia dicta itaque.
+      </p>
+    </div>
+
+    <div class="compoZone">
+      <InputText placeholder={"entrez votre nom"} />
+    </div>
+  </div>
+  <div class="displayCompoContain" id="checkbox">
+    <div class="text">
+      <h4>Checkbox</h4>
+      <p>Utilisation</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio
+        atque error, quos corrupti enim qui deserunt veniam perferendis
+        exercitationem sint asperiores, blanditiis accusantium iusto saepe
+        repudiandae eaque officia dicta itaque.
+      </p>
+    </div>
+
+    <div class="compoZone">
+      <Check sentenceA={"Parfait !!!"} sentenceB={"Veuillez cocher la case "} />
+    </div>
   </div>
 </section>
